32ef9fdca3874ad3dd06aac11ea530e4269e8705
Saul Carrillo <scd040725@hotmail.com>
2023-05-02 21:42:30 -0600

    se creo la vista del formuario todavia falta crear poner la cracion de texto para el formulario pero me que de en la clase 3 en el primer video

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import Header from './Componentes/Header/Header';
+import Formulario from './Componentes/Formulario';
 console.log(Header);
 
 function App() {
   return (
     <div className="App">
-      {Header()}
+      <Header/>
+      <Formulario/>
     </div>
   );
 }
