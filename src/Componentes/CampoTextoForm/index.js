ca4e57719bbc31d458a61a90b8d04f53d76c7dc4
Saul Carrillo <scd040725@hotmail.com>
2023-05-04 15:16:51 -0600

    todavia falta terminar la el ultimo video de la tercera clase y empezar la 4 clase

---
+++ src/Componentes/CampoTextoForm/index.js
@@ -0,0 +1,9 @@
+import "./CampoTextoForm.css"
+const CapoTexto = () =>{
+    return <div className="campo-texto">
+        <label >Nombre</label>
+        <input placeholder="Ingresa el nombre"/>
+    </div>
+}
+
+export default CapoTexto
--- src/Componentes/Formulario/index.js
+++ src/Componentes/Formulario/index.js
@@ -1,9 +1,13 @@
 import "./Formulario.css"
+import CapoTexto from "../CampoTextoForm"
 
 const Formulario = () =>{
     return <section className="formulario"> 
     <form>
         <h2>Rellena el formulario para crear el colaborador.</h2>
+        <CapoTexto />
+        <CapoTexto/>
+        <CapoTexto/>
     </form>
     </section>
 }
