0f4c4c3969941e5f8d24075909a0b31787417de8
Saul Carrillo <decas0742@gmail.com>
2023-05-10 22:43:39 -0600

    se termino la clase 5 y aprendimos concepto de usaState ternamirios para mandar o ejecutar acciones en nuestro codigo ya solo falta la clase 6 para terminar con el proyecto y me gusto mucho esta clase por que aprendi muchas cosas diferntes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 
 import './App.css';
+import { useState } from 'react';
 import Header from './Componentes/Header/Header';
 import Formulario from './Componentes/Formulario';
 import MiOrg from './Componentes/MiOrg';
 
 function App() {
+
+    const[mostrarFormulario, acturlizarMostrar] = useState(true)
+
+  const cambiarMostrar =() =>{
+    acturlizarMostrar(!mostrarFormulario)
+  }
+  //ternario--> condicion ? seMustra : noSeMuestra
+  // cortocircuito --> condicion && semuestra 
+
   return (
     <div className="App">
       <Header/>
-      <Formulario/>
-      <MiOrg/>
+      {/* {mostrarFormulario === true ? <Formulario/> : <div></div>} */}
+      {mostrarFormulario && <Formulario/>}
+      <MiOrg cambiarMostrar={cambiarMostrar}/>
     </div>
   );
 }
--- src/Componentes/MiOrg/index.js
+++ src/Componentes/MiOrg/index.js
@@ -1,9 +1,19 @@
 import "./MiOrg.css"
+import { useState } from "react"
 
-const MiOrg = () =>{
+const MiOrg = (props) =>{
+
+    // const [mostrar, actualizaMostrar] = useState(true)
+
+    // const manejoClick = () => {
+    //     console.log("ocultar/mostrar", !mostrar)
+    //     actualizaMostrar(!mostrar)
+    // }
+
+
     return <section className="organizacion-section">
         <h3 className="section-org">Mi Organización</h3>
-        <img src="/img/add-boton.png" alt="icon-add" className="add-img"/>
+        <img src="/img/add-boton.png" alt="icon-add" className="add-img" onClick={props.cambiarMostrar}/>
     </section>
 }
 
